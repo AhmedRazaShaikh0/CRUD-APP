9a3214218acc4b593e1e17d0ec9f2f4e45132e26
Ahmed Raza <ahmedraza6100@gmail.com>
2023-07-19 02:06:38 +0500

    Done with Crud Apis including GET, POST, PUT & DELETE

---
+++ Database/Connection.mjs
@@ -0,0 +1,15 @@
+import { connect } from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await connect('mongodb+srv://ahmedraza:ahmedraza12345@cluster0.ykj1f2q.mongodb.net', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+  }
+};
+
+export default connectDB;
